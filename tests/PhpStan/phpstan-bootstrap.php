12ce8a976eed7df3988968b3c3c8fc2c50906723
Michael Keiluweit <michael.keiluweit@oxid-esales.com>
2026-06-18 15:37:21 +0200

    fix: phpstan User_parent alias + permission contract test (OXS-3059/3060)

--- tests/PhpStan/phpstan-bootstrap.php
+++ tests/PhpStan/phpstan-bootstrap.php
@@ -12,3 +12,9 @@ class_alias(
     \OxidEsales\Eshop\Application\Controller\Admin\NavigationController::class,
     \OxidSupport\Heartbeat\Shared\Controller\Admin\NavigationController_parent::class
 );
+
+// Create class alias for the User extension parent (OXS-3060 service-user override)
+class_alias(
+    \OxidEsales\Eshop\Application\Model\User::class,
+    \OxidSupport\Heartbeat\Shop\Extend\Application\Model\User_parent::class
+);
